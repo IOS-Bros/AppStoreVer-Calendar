134d9dd1ed0375b25479870ca9bb9a7e2c3ab0e5
jasper <63331153+jasper-oh@users.noreply.github.com>
2021-07-25 12:45:26 +0900

    start

--- JasperLaboratory/CalendarLab01/CalendarLab01/ViewController.swift
+++ JasperLaboratory/CalendarLab01/CalendarLab01/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import CalendarLib
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myCalendar: MGCMonthPlannerView!
+    @IBOutlet weak var lblExample: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        myCalendar.dateFormat = "d"
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(clickActionHere))
+        lblExample.isUserInteractionEnabled = true
+        lblExample.addGestureRecognizer(tap)
+        
     }
 
-
+    
+    @objc
+    func clickActionHere(sender: UITapGestureRecognizer){
+        print("HELLEJWALKDNvasdasd")
+    }
+    
+    
+    @IBAction func AddAction(_ sender: UIButton) {
+        print("ASDaslkjdh")
+    }
+        
+    
 }
 
